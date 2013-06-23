dae73933d06df1d54f6a483360c326b550a6b3b6
paul casey <paulcaseys@gmail.com>
2013-06-23 15:53:12 +1000

    adding grand central dispatch queues to increase percieved load speeds

--- iOSApp/MasterViewController.m
+++ iOSApp/MasterViewController.m
@@ -115,33 +115,46 @@ int section;
 // example to parse cosmos
 - (void)initialiseCosmosFeedLoad {
 	
-    // need to parse the url because pipes in the url cause errors
-    NSString *unDecodedURL =[NSString stringWithFormat:@"http://cosmos.is/api/service/data/format/json/?project_name=SummerAtTarget&project_password=6CB4816A23A965B5DFD58E45F4C23&table=unique_references&batch=1&batchSize=6&whereConditionArray=project_id||9&select=*&orderBy=vote_count||desc"];
-    NSURL *decodedUrl = [NSURL URLWithString:[unDecodedURL stringByAddingPercentEscapesUsingEncoding: NSUTF8StringEncoding]];
-    NSData *url = [NSData dataWithContentsOfURL:decodedUrl];
+    // creates an async queue, so the page can be displayed before loading is complete
+    dispatch_queue_t feedQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0ul);
+    dispatch_async(feedQueue, ^{
+        
+        // need to parse the url because pipes in the url cause errors
+        NSString *unDecodedURL =[NSString stringWithFormat:@"http://cosmos.is/api/service/data/format/json/?project_name=SummerAtTarget&project_password=6CB4816A23A965B5DFD58E45F4C23&table=unique_references&batch=1&batchSize=6&whereConditionArray=project_id||9&select=*&orderBy=vote_count||desc&rand=1"];
+        NSURL *decodedUrl = [NSURL URLWithString:[unDecodedURL stringByAddingPercentEscapesUsingEncoding: NSUTF8StringEncoding]];
+        NSData *url = [NSData dataWithContentsOfURL:decodedUrl];
+        
+        
+        NSError *err;
+        
+        NSDictionary* json1 = [NSJSONSerialization
+                               JSONObjectWithData:url
+                               options:kNilOptions
+                               error:&err];
+        
+        // FIRST GROUP OF ITEMS
+        for (NSMutableDictionary *itemFromGroup1 in json1){
+            // you could set up a condition here to detect which category it is in, then define which array to place it in
+            [self parseAndPushCosmosItemIntoArray:itemFromGroup1 theArray:_firstItemsArray];
+        }
+        // SECOND GROUP OF ITEMS (this could be from a different feed)
+        for (NSMutableDictionary *itemFromGroup2 in json1){
+            // you could set up a condition here to detect which category it is in, then define which array to place it in
+            [self parseAndPushCosmosItemIntoArray:itemFromGroup2 theArray:_secondItemsArray];
+        }
+        // now put them into the _dataArray
+        [_dataArray addObject:[NSDictionary dictionaryWithObject:_firstItemsArray forKey:@"data"]];
+        [_dataArray addObject:[NSDictionary dictionaryWithObject:_secondItemsArray forKey:@"data"]];
+        
+        
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            NSLog(@"loading complete!");
+            // reload table
+            [tableView reloadData];
+        });
+        
+    });
     
-    NSError *err;
-    NSDictionary* json1 = [NSJSONSerialization
-                           JSONObjectWithData:url
-                           options:kNilOptions
-                           error:&err];
-    
-    // FIRST GROUP OF ITEMS
-    for (NSMutableDictionary *itemFromGroup1 in json1){
-        // you could set up a condition here to detect which category it is in, then define which array to place it in
-        [self parseAndPushCosmosItemIntoArray:itemFromGroup1 theArray:_firstItemsArray];
-    }
-    // SECOND GROUP OF ITEMS (this could be from a different feed)
-    for (NSMutableDictionary *itemFromGroup2 in json1){
-        // you could set up a condition here to detect which category it is in, then define which array to place it in
-        [self parseAndPushCosmosItemIntoArray:itemFromGroup2 theArray:_secondItemsArray];
-    }
-    // now put them into the _dataArray
-    [_dataArray addObject:[NSDictionary dictionaryWithObject:_firstItemsArray forKey:@"data"]];
-    [_dataArray addObject:[NSDictionary dictionaryWithObject:_secondItemsArray forKey:@"data"]];
-    
-    
-	texter.text = @"...";
 }
 
 
